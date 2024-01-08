ef5d69a880cd3eb136b2995edb49c2e1ad787907
Dinesh Chandra <chandv10@gene.com>
2024-01-08 11:42:11 +0530

    Added api's for product,productcategory,productsubcategory,testtypes

---
+++ product/filters.py
@@ -0,0 +1,39 @@
+import django_filters
+from rest_framework import filters
+from .models import TestType, ProductCategory, Product, ProductCategoryPrompt, ProductCategoryPromptCode, ProductPrompt, ProductSubCategory, Prompt
+
+class TestTypeFilter(django_filters.FilterSet):
+
+    id = django_filters.NumberFilter(lookup_expr='exact')
+    code = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = TestType
+        fields = ['id', 'code']
+
+class ProductCategoryFilter(django_filters.FilterSet):
+
+    id = django_filters.NumberFilter(lookup_expr='exact')
+    category = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = ProductCategory
+        fields = ['id', 'category']
+
+class ProductSubCategoryFilter(django_filters.FilterSet):
+
+    id = django_filters.NumberFilter(lookup_expr='exact')
+    sub_category = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = ProductSubCategory
+        fields = ['id', 'sub_category']
+
+class ProductFilter(django_filters.FilterSet):
+
+    id = django_filters.NumberFilter(lookup_expr='exact')
+    product_code = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Product
+        fields = ['id', 'product_code']
