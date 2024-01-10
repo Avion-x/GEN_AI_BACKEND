9814748a2a03cd05f4d3e7ede4f7c69627b738ea
Damodar Reddy Veluru <velurud@gene.com>
2024-01-10 13:47:30 +0530

    added child count sto show in fronend

--- product/serializers.py
+++ product/serializers.py
@@ -9,18 +9,27 @@ class TestTypeSerializer(serializers.ModelSerializer):
         model = TestType
         fields = '__all__'
 
-class ProductCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductCategory
-        fields = '__all__'
-
-class ProductSubCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductSubCategory
-        fields = '__all__'
-
 class ProductSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Product
         fields = '__all__'
 
+class ProductSubCategorySerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
+    class Meta:
+        model = ProductSubCategory
+        fields = '__all__'
+
+    def get_product_count(self, obj):
+        return obj.product.count()
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    sub_category_count = serializers.SerializerMethodField()
+    class Meta:
+        model = ProductCategory
+        # fields = '__all__'
+        exclude = ['prompts']
+
+    def get_sub_category_count(self, obj):
+        return obj.product_sub_category.count()
