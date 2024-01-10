09f13fee79afdfc1eeb3fe62faf4ee90c22f18f4
Dinesh Chandra <chandv10@gene.com>
2024-01-10 20:42:09 +0530

    Added parent names in api.

--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import TestType, ProductCategory, Product, ProductCategoryPrompt, ProductCategoryPromptCode, ProductPrompt, ProductSubCategory, Prompt
-from product.models import TestType
 from django.contrib.auth.hashers import make_password
 
 
@@ -10,22 +9,42 @@ class TestTypeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
-    
+    sub_category_name = serializers.SerializerMethodField()
+    category_name = serializers.SerializerMethodField()
+    customer_name = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
 
+    def get_sub_category_name(self, obj):
+        return obj.product_sub_category.sub_category
+    
+    def get_category_name(self, obj):
+        return obj.product_sub_category.product_category.category
+    
+    def get_customer_name(self, obj):
+        return obj.customer.name
+
 class ProductSubCategorySerializer(serializers.ModelSerializer):
     product_count = serializers.SerializerMethodField()
+    category_name = serializers.SerializerMethodField()
+    customer_name = serializers.SerializerMethodField()
     class Meta:
         model = ProductSubCategory
         fields = '__all__'
 
     def get_product_count(self, obj):
         return obj.product.count()
+    
+    def get_category_name(self, obj):
+        return obj.product_category.category
+    
+    def get_customer_name(self, obj):
+        return obj.customer.name
 
 class ProductCategorySerializer(serializers.ModelSerializer):
     sub_category_count = serializers.SerializerMethodField()
+    customer_name = serializers.SerializerMethodField()
     class Meta:
         model = ProductCategory
         # fields = '__all__'
@@ -33,3 +52,6 @@ class ProductCategorySerializer(serializers.ModelSerializer):
 
     def get_sub_category_count(self, obj):
         return obj.product_sub_category.count()
+    
+    def get_customer_name(self, obj):
+        return obj.customer.name
