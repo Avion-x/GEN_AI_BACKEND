b7d8833f5650a22d9b0149c0fe03619f1401f652
Dinesh Chandra <chandv10@gene.com>
2024-03-21 13:22:28 +0530

    Fixed a bug in product serializers.

--- user/settings.py
+++ user/settings.py
@@ -192,6 +192,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 
